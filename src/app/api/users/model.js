fc488aef061b7e2e7126c134ce9ec56bc5014ee2
Bladimir Cabrera <vladwithcode@gmail.com>
2025-02-12 09:10:54 -0600

    chore: use consistent indentation

--- src/app/api/users/model.js
+++ src/app/api/users/model.js
@@ -1,37 +1,37 @@
 import { Schema, model, models } from "mongoose";
 
 const UsuarioSchema = new Schema({
-  nombre: {
-    type: String,
-    required: true,
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  departamento: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  role: {
-    type: String,
-    enum: ["admin", "user"],
-    default: "user",
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
+    nombre: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    departamento: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user",
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
 });
 
 export default models.User || model("User", UsuarioSchema);
