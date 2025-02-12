546ccb569f27cfb86989b4542a3962bb73eeef62
Bladimir Cabrera <vladwithcode@gmail.com>
2025-02-11 19:13:04 -0600

    chore: add user endpoints

---
+++ src/app/api/users/[id]/route.js
@@ -0,0 +1,89 @@
+export async function GET(_, { params }) {
+    await connectDB()
+    const { id } = await params;
+
+    const usuario = await User.findById(id);
+
+    if (!usuario) {
+        return new NextResponse.json(
+            {
+                message: "Usuario no encontrado",
+                error: "Usuario no encontrado"
+            },
+            { status: 404 }
+        );
+    }
+
+    return new NextResponse.json(
+        {
+            message: "Usuario encontrado",
+            usuario
+        },
+        { status: 200 }
+    );
+}
+
+export async function PUT(req, { params }) {
+    await connectDB()
+    const {
+        nombre,
+        username,
+        password,
+        departamento,
+        role,
+        email,
+    } = await req.json();
+    const { id } = await params;
+
+    let newPass;
+    if (password) {
+        try {
+            newPass = await bcrypt.hash(password, 10);
+        } catch (err) {
+            return new NextResponse.json(
+                {
+                    message: "Error al encriptar la contraseña",
+                    error: err
+                },
+                { status: 500 }
+            );
+        }
+    }
+
+    try {
+        await User.updateOne(
+            { _id: id },
+            {
+                $set: {
+                    nombre,
+                    username,
+                    departamento,
+                    role,
+                    email,
+                    password: newPass
+                }
+            }
+        );
+
+        return new NextResponse.json(
+            {
+                message: "Usuario actualizado correctamente",
+            },
+            { status: 200 }
+        );
+    } catch (err) {
+        return new NextResponse.json(
+            {
+                message: "Error al actualizar el usuario",
+                error: err
+            },
+            { status: 500 }
+        );
+    }
+}
+
+export async function DELETE(_, { params }) {
+    await connectDB()
+    const { id } = await params;
+    await User.deleteOne({ _id: id });
+}
--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,13 +1,78 @@
-import connectDB from "@/app/db/db";
+import bcrypt from "bcrypt";
 import { NextResponse } from "next/server";
+import connectDB from "@/app/db/db";
+import User from "./model";
+
+const DEFAULT_LIMIT = 10;
 
 export async function GET(req) {
-  try {
-    await connectDB();
-  } catch (err) {
-    return new NextResponse(err.message, { status: 500 });
-  }
-  const msg = req.nextUrl.searchParams.msg || "Hello World!";
+    await connectDB()
+    const limit = req.nextUrl.searchParams.limit || DEFAULT_LIMIT;
 
-  return new NextResponse(msg);
+    const usuarios = await User
+        .find({ password: false })
+        .limit(limit)
+        .skip(req.nextUrl.searchParams.offset || 0);
+
+    return new NextResponse.json(
+        {
+            message: "Listado de usuarios",
+            usuarios
+        },
+        { status: 200 }
+    );
 }
+
+export async function POST(req) {
+    await connectDB()
+    const {
+        nombre,
+        username,
+        password,
+        departamento,
+        role,
+        email
+    } = await req.json();
+
+    let hashedPw;
+
+    try {
+        hashedPw = await bcrypt.hash(password, 10);
+    } catch (err) {
+        return new NextResponse.json(
+            {
+                message: "Error al encriptar la contraseña",
+                error: err
+            },
+            { status: 500 }
+        );
+    }
+
+    const usuario = new User({
+        nombre,
+        username,
+        departamento,
+        role,
+        email
+    });
+
+    try {
+        const u = await usuario.save();
+        return new NextResponse.json(
+            {
+                message: "Usuario creado correctamente",
+                id: u._id,
+            },
+            { status: 201 }
+        )
+    } catch (err) {
+        return new NextResponse.json(
+            {
+                message: "Error al crear el usuario",
+                error: err
+            },
+            { status: 500 }
+        );
+    }
+}
+
