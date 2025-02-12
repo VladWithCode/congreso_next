28d163d70fc7754502f0fb8e523064af32aca714
Bladimir Cabrera <vladwithcode@gmail.com>
2025-02-12 17:06:13 -0600

    feat: add file uploads

--- src/app/api/documents/[id]/version/route.js
+++ src/app/api/documents/[id]/version/route.js
@@ -1,38 +1,108 @@
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import p from "node:path";
+import { writeFile } from "node:fs/promises";
+import { Types } from "mongoose";
+import connectDB from "@/app/db/db";
+import "@/app/api/departments/model";
+import Doc from "../../model";
+import { DOC_PUBLIC_PATH } from "../../route";
+
 export async function PUT(req, { params }) {
     await connectDB()
+    const cookieStore = await cookies()
+
     const [
-        { version, date, filename },
+        data,
         { id },
     ] = await Promise.all([
-        req.json(),
+        req.formData(),
         params,
     ]);
-    const author = req.cookie.get("userId");
+    const file = data.get("file");
+    const author = cookieStore.get("userId").value;
+
+    let doc;
+    try {
+        doc = await Doc.findById({ _id: id }).populate("department");
+    } catch (err) {
+        console.log(err)
+        return NextResponse.json(
+            {
+                message: "Error al recuperar el documento",
+                error: err
+            },
+            { status: 500 }
+        );
+    }
+
+    if (!doc) {
+        return NextResponse.json(
+            {
+                message: "Documento no encontrado",
+                error: "document not found"
+            },
+            { status: 404 }
+        );
+    }
+    
+    let sectionName = "";
+    for (let s of doc.department.sections) {
+        if (s._id.toString() === doc.section.toString()) {
+            sectionName = s.name;
+            break;
+        }
+    }
+
+    const date = data.get("date") || Date.now();
+    const version = data.get("version") || doc.currentVersion.version + 1;
+    const safeDept = doc.department.name.replace(/[^a-zA-Z0-9]/g, "-");
+    const safeSection = sectionName.replace(/[^a-zA-Z0-9]/g, "-");
+    const finalFilename = `${safeDept}-${safeSection}-${doc.key}-${doc.name}-v${version}${p.extname(file.name)}`;
 
     try {
+        const arrBuf = await file.arrayBuffer();
+        const buf = Buffer.from(arrBuf);
+        await writeFile(p.join(DOC_PUBLIC_PATH, finalFilename), buf);
+    } catch (err) {
+        console.error(err);
+        return NextResponse.json(
+            {
+                message: "Error al guardar el archivo",
+                error: err
+            },
+            { status: 500 }
+        );
+    }
+
+    try {
+        const versionId = new Types.ObjectId();
         await Doc.updateOne(
             { _id: id },
             {
                 $set: {
                     currentVersion: {
+                        _id: versionId,
                         version,
                         date,
                         author,
-                        filename,
-                    }
+                        filename: finalFilename,
+                    },
+                    filename: finalFilename,
                 },
                 $push: {
                     versions: {
-                        // Needed for $sort
+                        // needed for $sort
                         $each: [
                             {
+                                _id: versionId,
                                 version,
                                 date,
                                 author,
-                                filename,
+                                filename: finalFilename,
                             }
                         ],
-                        // Ensure the array remains sorted by version DESC
+                        // ensure the array remains sorted by version desc
                         $sort: {
                             version: -1,
                         }
@@ -43,11 +113,12 @@ export async function PUT(req, { params }) {
 
         return NextResponse.json(
             {
-                message: "El archivo para el documento fue actualizado correctamente",
+                message: "Archivo para el documento actualizado correctamente",
             },
             { status: 200 }
         );
     } catch (err) {
+        console.log(err)
         return NextResponse.json(
             {
                 message: "Error al actualizar el archivo para el documento",
@@ -76,6 +147,9 @@ export async function DELETE(req, { params }) {
                     versions: {
                         _id: fileId
                     }
+                },
+                $set: {
+                    currentVersion: "$versions.$[0]",
                 }
             }
         );
--- src/app/api/documents/route.js
+++ src/app/api/documents/route.js
@@ -1,5 +1,14 @@
+import { NextResponse } from "next/server";
+import { Types } from "mongoose";
+import p from "node:path";
+import { writeFile } from "node:fs/promises";
+import path from "node:path";
 import connectDB from "@/app/db/db";
 import Doc from "./model";
+import Department from "@/app/api/departments/model";
+import { cookies } from "next/headers";
+
+export const DOC_PUBLIC_PATH = process.env.PUBLIC_PATH || "./public/uploads";
 
 export async function GET(req) {
     await connectDB()
@@ -21,28 +30,95 @@ export async function GET(req) {
 
 export async function POST(req) {
     await connectDB()
-    const {
-        name,
-        department,
-        section,
-        filename,
-    } = await req.json();
+    const cookieStore = await cookies()
+
+    const data = await req.formData();
+    const name = data.get("name");
+    const departmentId = data.get("department");
+    const sectionId = data.get("section");
+    const key = data.get("key");
+    const author = cookieStore.get("userId").value;
+    /** @type {File} */
+    const file = data.get("file");
+    const ext = p.extname(file.name)
+
+    let dept;
+    try {
+        dept = await Department.findById(departmentId);
+    } catch (err) {
+        return NextResponse.json(
+            {
+                message: "Error al recuperar el departamento",
+                error: "query error"
+            },
+            { status: 500 }
+        );
+    }
+    if (!dept) {
+        return NextResponse.json(
+            {
+                message: "Departamento no encontrado",
+                error: "invalid department"
+            },
+            { status: 400 }
+        );
+    }
+    let section;
+    for (let s of dept.sections) {
+        if (s._id.toString() === sectionId) {
+            section = s;
+            break;
+        }
+    }
+    if (!section) {
+        return NextResponse.json(
+            {
+                message: "Sección no encontrada",
+                error: "invalid section"
+            },
+            { status: 400 }
+        );
+    }
 
     const currentVersion = {
+        _id: new Types.ObjectId(),
         version: 1,
         date: Date.now(),
-        author: req.cookie.get("userId"),
-        filename,
+        author,
+        filename: "",
     }
 
     const doc = new Doc({
+        _id: new Types.ObjectId(),
         name,
-        department,
-        section,
-        filename,
-        currentVersion: currentVersion,
-        versions: [currentVersion],
+        department: departmentId,
+        section: sectionId,
+        key,
+        currentVersion: {},
+        versions: [],
     });
+
+    const arrBuf = await file.arrayBuffer()
+    const buf = Buffer.from(arrBuf)
+    const safeDept = dept.name.replace(/[^a-zA-Z0-9]/g, "-");
+    const safeSection = section.name.replace(/[^a-zA-Z0-9]/g, "-");
+    const finalFilename = `${safeDept}-${safeSection}-${key}-${name}-v${currentVersion.version}${ext}`
+    try {
+        await writeFile(path.join(DOC_PUBLIC_PATH, finalFilename), buf);
+    } catch (err) {
+        console.error(err)
+        return NextResponse.json(
+            {
+                message: "Error al guardar el archivo",
+                error: err
+            },
+            { status: 500 }
+        );
+    }
+    doc.filename = finalFilename;
+    currentVersion.filename = finalFilename;
+    doc.currentVersion = currentVersion;
+    doc.versions.push(currentVersion);
 
     try {
         const d = await doc.save();
