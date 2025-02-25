d7086ad59db00c20f3ea53489097fd3cc0b967e2
Bladimir Rangel <vladwithcode@gmail.com>
2025-02-25 16:40:37 -0600

    chore: remove unused service

--- src/app/usuarios/service.js
+++
@@ -1,8 +0,0 @@
-export async function getUsers() {
-  const response = await fetch("/api/users?msg=test");
-  if (!response.ok) {
-    throw new Error("Error al recuperar usuarios: " + response.status);
-  }
-
-  return await response.text();
-}
